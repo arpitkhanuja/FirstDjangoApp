7df0574eece3e075b58ad02211b9224c11e584c7
Arpit Khanuja <khanujaarpit@gmail.com>
2015-06-09 00:37:16 -0700

    Added initial files

---
+++ functional_tests.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox() # opens a Firefox browser window
+browser.get('http://locahlost:8000') # opens the web page we want to test
+
+assert 'Django' in browser.title # checks whether or not 'Django' is in the browser title
